5ac94a41c0151d4a36d76578fd78b34ad31aaa3e
alicecamargo <aliceisabelle523@gmail.com>
2022-11-16 22:30:29 -0300

    ESSA DAQUI

--- python/aaa.py
+++ python/aaa.py
@@ -8,11 +8,12 @@ class Application():
     def __init__(self):
         self.root = root
         self.base()
+        self.frames_da_tela()
         root.mainloop()
 
     def base(self):
         self.root.title("INTERFACE BASE")
-        self.root.configure(background='lightpink')
+        self.root.configure(background='lightgreen')
         self.root.geometry("800x600")
         self.root.resizable(True, True)
         self.root.maxsize(width=1000, height=1000)
@@ -20,6 +21,7 @@ class Application():
 
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.5)
 
 
 Tk(Application())
